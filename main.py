b2163dc85277ad12fcc0b52e7123bcf29a1e3f46
Ruslan Fatkulin <rusya86hmrus@gmail.com>
2022-12-03 19:28:25 +0500

    start_dev3

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from aiogram.dispatcher.filters import Text
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 dotenv_path = os.path.join(os.path.dirname(__file__), 'env.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
@@ -13,16 +14,27 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message):
-    KB = ReplyKeyboardMarkup(resize_keyboard=True)
-    KB.row(
-        KeyboardButton(text="О мероприятии"),
-        types.KeyboardButton(text="Программа мероприятия"))
-    KB.row(types.KeyboardButton(text="Программа мероприятия"))
-    KB.row(
-        types.KeyboardButton(text="Спикеры"),
-        types.KeyboardButton(text='Задать вопрос'))
+    KB = InlineKeyboardMarkup(resize_keyboard=True)
+    buttons = [InlineKeyboardButton('О мероприятии', callback_data='About Event'), InlineKeyboardButton(text='Спикеры', callback_data='Spickers'), InlineKeyboardButton(text='Программа мероприятия', callback_data='Programm event')]
+    KB.add(*buttons)
+    await message.reply("Выберете нужный раздел", reply_markup=KB)
+
+@dp.callback_query_handler()
+async def Menu(query: types.CallbackQuery):
+    if query.data == 'About Event':
+        KB = InlineKeyboardMarkup(resize_keyboard=True).add(InlineKeyboardButton(text='Вернуться в меню', callback_data='start'))
+        await query.message.reply("ITFESTINFO", reply_markup=KB)
+        
+    if query.data == 'Spickers':
+        KB = InlineKeyboardMarkup(resize_keyboard=True).add(InlineKeyboardButton(text='Вернуться в меню', callback_data='start'))
+        await query.message.reply("Спикеры", reply_markup=KB)
+        
+    if query.data == 'Programm event':
+        KB = InlineKeyboardMarkup(resize_keyboard=True).add(InlineKeyboardButton(text='Вернуться в меню', callback_data='start'))
+        await query.message.reply("Программа мероприятия", reply_markup=KB)
+        
     
-    await message.answer("Пожалуйста, выберете интересующий Вас раздел", reply_markup=KB)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
